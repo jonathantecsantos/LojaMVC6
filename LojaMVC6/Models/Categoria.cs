08678704492bab0956c7b008d0081c361743185b
Jonathan <jonathan.tecsantos@gmail.com>
2021-11-22 20:30:09 -0300

    Criando classes de dominio - Categoria e Lanche / Removendo arquivo default de erro.

---
+++ LojaMVC6/Models/Categoria.cs
@@ -0,0 +1,9 @@
+﻿namespace LojaMVC6.Models;
+
+public class Categoria
+{
+    public int CategoriaId { get; set; }
+    public string CategoriaNome { get; set; }
+    public string Descricao { get; set; }
+    public List<Lanche> Lanches { get; set; }
+}
